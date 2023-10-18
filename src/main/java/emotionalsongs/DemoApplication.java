e20676325df9080f833b34cb2fee101c35bb47aa
danielemaccagnan <63585453+th1italy@users.noreply.github.com>
2023-10-18 22:21:21 +0200

    initial commit

--- src/main/java/emotionalsongs/DemoApplication.java
+++ src/main/java/emotionalsongs/DemoApplication.java
@@ -1,10 +1,13 @@
-package com.example.demo;
+package emotionalsongs;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+@Theme(value= "mytodo")
+public class DemoApplication implements AppShellConfigurator {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
