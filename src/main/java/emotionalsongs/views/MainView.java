9e2dec65954205468b3a40a6eb81349f57513943
danielemaccagnan <63585453+th1italy@users.noreply.github.com>
2023-10-18 23:17:57 +0200

    initial commit

--- src/main/java/emotionalsongs/views/MainView.java
+++ src/main/java/emotionalsongs/views/MainView.java
@@ -23,7 +23,7 @@ public class MainView extends VerticalLayout {
         addButton.addClickShortcut(Key.ENTER);
 
         add(
-                new H1("Vaadin Todo"),
+                new H1("calabria Todo"),
                 todosList,
                 new HorizontalLayout(
                         taskField,
