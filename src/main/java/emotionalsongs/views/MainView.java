5aefff3005149b0bde1ac62b09ad8e63ae94d07c
danielemaccagnan <63585453+th1italy@users.noreply.github.com>
2023-10-20 18:42:47 +0200

    initial commit

--- src/main/java/emotionalsongs/views/MainView.java
+++ src/main/java/emotionalsongs/views/MainView.java
@@ -1,34 +1,45 @@
 package emotionalsongs.views;
 
-import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.checkbox.Checkbox;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.Scroller;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        VerticalLayout todosList = new VerticalLayout();
-        TextField taskField = new TextField();
-        Button addButton = new Button("Add");
-        addButton.addClickListener(click -> {
-            Checkbox checkbox = new Checkbox(taskField.getValue());
-            todosList.add(checkbox);
-        });
-        addButton.addClickShortcut(Key.ENTER);
+        setJustifyContentMode(JustifyContentMode.END);
+        setPadding(true);
+        getStyle().set("background-color", "#C2C2D6");
 
-        add(
-                new H1("calabria Todo"),
-                todosList,
-                new HorizontalLayout(
-                        taskField,
-                        addButton
-                )
-        );
+        // Crea un'icona personalizzata dall'immagine
+        Image customIcon = new Image("images/logo.png", "logo");
+        customIcon.setHeight("100px");
+        customIcon.setWidth("100px");
+
+        // Crea un testo leggermente più grande utilizzando il componente Span
+        Span text = new Span("EmotionalSongs");
+        text.getElement().getStyle().set("font-size", "1.2em");
+
+        // Crea un layout orizzontale per l'icona e il testo
+        HorizontalLayout headerLayout = new HorizontalLayout(customIcon, text);
+        headerLayout.setAlignItems(Alignment.CENTER);
+
+        // Crea un pulsante "Login" con un'icona dalla libreria Vaadin
+        Button loginButton = new Button("Login");
+        loginButton.setIcon(new Icon(VaadinIcon.USER));
+
+        // Aggiungi gli elementi al layout
+        add(headerLayout, loginButton);
+
+
+        // Allinea il pulsante "Login" in alto a destra
+        setHorizontalComponentAlignment(Alignment.END, loginButton);
     }
 }
