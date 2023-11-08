9505a3e496f1efde86d2d868973b8b8a1b493a29
burgosale <sandro.bugno@protonmail.com>
2023-11-08 15:04:15 +0100

    burgos initial master commit

--- frontend/generated/vaadin.ts
+++ frontend/generated/vaadin.ts
@@ -3,7 +3,3 @@ import './vaadin-featureflags.js';
 import './index';
 
 import 'Frontend/generated/jar-resources/vaadin-dev-tools/vaadin-dev-tools.js';
-
-import './theme-mytodo.global.generated.js';
-import { applyTheme } from './theme.js';
-applyTheme(document);
