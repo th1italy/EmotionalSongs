9c438b0516fd70945e6498ec10d1905561e0db2b
burgosale <sandro.bugno@protonmail.com>
2023-11-14 00:30:17 +0100

    conf and entities

--- src/main/java/emotionalsongs/DemoApplication.java
+++ src/main/java/emotionalsongs/DemoApplication.java
@@ -4,8 +4,14 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * @author Alessandro Bugno
+ * @author Daniele Maccagna
+ * @author Tommaso Mariani
+ */
+@SpringBootApplication()
 @Theme(value= "")
 public class DemoApplication implements AppShellConfigurator {
 
