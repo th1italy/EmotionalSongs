439043d10516214876f963ec3ca8d66506b08385
burgosale <sandro.bugno@protonmail.com>
2023-11-15 08:51:29 +0100

    conf and entities

--- src/main/java/emotionalsongs/views/MainView.java
+++ src/main/java/emotionalsongs/views/MainView.java
@@ -1,72 +1,43 @@
 package emotionalsongs.views;
 
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.PasswordField;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility;
 
 @Route("")
-public class MainView extends AppLayout {
+public class MainView extends VerticalLayout {
 
     public MainView() {
-        H1 appName = new H1("EmotionalSongs");
-        Icon logo = new Icon("images/codicefiscale.png");
-        logo.setSize("40px");
-        logo.setSize("40px");
-        appName.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.NONE);
-        Header header = new Header(logo, appName);
+        setJustifyContentMode(JustifyContentMode.END);
+        setPadding(true);
+        getStyle().set("background-color", "#C2C2D6");
 
+        // Crea un'icona personalizzata dall'immagine
+        Image customIcon = new Image("images/logo.png", "logo");
+        customIcon.setHeight("100px");
+        customIcon.setWidth("100px");
 
-        addToDrawer(header,  new Footer());
+        // Crea un testo leggermente più grande utilizzando il componente Span
+        Span text = new Span("EmotionalSongs");
+        text.getElement().getStyle().set("font-size", "1.2em");
 
+        // Crea un layout orizzontale per l'icona e il testo
+        HorizontalLayout headerLayout = new HorizontalLayout(customIcon, text);
+        headerLayout.setAlignItems(Alignment.CENTER);
 
+        // Crea un pulsante "Login" con un'icona dalla libreria Vaadin
         Button loginButton = new Button("Login");
         loginButton.setIcon(new Icon(VaadinIcon.USER));
-        loginButton.getElement().getStyle().set("margin-left", "auto"); // Sposta a destra
 
-        loginButton.addClickListener(event -> openLoginDialog());
+        // Aggiungi gli elementi al layout
+        add(headerLayout, loginButton);
 
-        addToNavbar(loginButton);
-    }
 
-    private void openLoginDialog() {
-        Dialog loginDialog = new Dialog();
-        loginDialog.setCloseOnEsc(false);
-        loginDialog.setCloseOnOutsideClick(false);
-
-        // Aggiungi i campi di testo per username e password
-        TextField usernameField = new TextField("Username");
-        PasswordField passwordField = new PasswordField("Password");
-
-        // Aggiungi un pulsante per confermare il login
-        Button confirmButton = new Button("Login");
-        confirmButton.getElement().getThemeList().add("primary");
-
-        // Aggiungi un'azione quando si preme il pulsante di conferma
-        confirmButton.addClickListener(event -> {
-            // Qui puoi aggiungere la logica di autenticazione
-            // e chiudere la finestra di dialogo se l'autenticazione ha successo
-            loginDialog.close();
-        });
-
-        // Aggiungi un link per la registrazione
-        Button registerButton = new Button("Nuovo? Registrati");
-        registerButton.addClickListener(event -> {
-            UI.getCurrent().navigate(RegistrazioneView.class);
-            loginDialog.close();
-            // Potresti chiudere anche la finestra di login prima di aprire quella di registrazione
-        });
-
-        loginDialog.add(usernameField, passwordField, confirmButton, registerButton);
-        loginDialog.open();
+        // Allinea il pulsante "Login" in alto a destra
+        setHorizontalComponentAlignment(Alignment.END, loginButton);
     }
 }
