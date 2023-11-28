6767cc8a835478ba96ab73486a5b322c3e0cea6b
burgosale <sandro.bugno@protonmail.com>
2023-11-28 15:59:56 +0100

    a

---
+++ src/main/java/emotionalsongs/backend/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package emotionalsongs.backend.repositories;
+
+import emotionalsongs.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
